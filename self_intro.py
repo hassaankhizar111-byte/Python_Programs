81078d7dbee427c4fafd1bc06ff91a3e6d0f13bc
Hassaan Shariq <hassaankhizar111@gmail.com>
2026-01-18 23:15:21 +0500

    Create self_intro.py

---
+++ self_intro.py
@@ -0,0 +1,17 @@
+def self_introduction():
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
+    country = input("Enter your country: ")
+    skill = input("Enter your skill: ")
+    favourite_car = input("Enter your favourite car: ")
+
+    print("\n--- self Introduction ---")
+    print(f"my name is {name}.")
+    print(f"I am {age} years old with big dreams.")
+    print(f"I live in {country}.")
+    print(f"I am learning a very valuable skill: {skill}.")
+    print("my goal is to earn from development, cloud computing, and business.")
+    print(f"i want buy my favourite car: {favourite_car}.")
+    print("Most importantly, I want to make my family proud and happy.")
+
+self_introduction()
