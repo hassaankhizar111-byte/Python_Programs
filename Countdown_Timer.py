7c940a0b965941eef25e07b3fca1133a492717f4
Hassaan Shariq <hassaankhizar111@gmail.com>
2026-05-09 13:34:50 +0500

    Update Countdown_Timer.py

--- Countdown_Timer.py
+++ Countdown_Timer.py
@@ -5,5 +5,7 @@ seconds = int(input("Seconds: "))
 for i in range(seconds, 0, -1):
     print(i)
     time.sleep(1)
+    if i < 10:
+        print("Hurry up! Only", i, "seconds left!")
 
 print("Time's up!")
