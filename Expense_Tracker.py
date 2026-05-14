bc8671aed279faf1b7e062f83528b74926ed4449
Hassaan Shariq <hassaankhizar111@gmail.com>
2026-05-14 22:39:17 +0500

    Update Expense_Tracker.py

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -1,14 +1,33 @@
-# Monthly Expense Tracker without OOPs
+def get_expenses(expense_name):
+    amount = input(f"Enter your monthly {expense_name} expenses: ")
+    return amount
 
 monthly_income = int(input("Enter your monthly income: "))
-monthly_expenses = int(input("Enter your monthly expenses:"))
+
+clothing_expenses = get_expenses("clothing")
+food_expenses = get_expenses("food")
+rent_expenses = get_expenses("rent")
+transportation_expenses = get_expenses("transportation")
+
+monthly_expenses = (
+    int(clothing_expenses) + 
+    int(food_expenses) + 
+    int(rent_expenses) + 
+    int(transportation_expenses)
+)
+
 savings = monthly_income - monthly_expenses
-print(f"Your monthly savings are: {savings}")
-spending_percentage = (monthly_expenses / monthly_income) * 100
-print(f"You are spending {spending_percentage:.2f}% of your income.")
-if spending_percentage > 50:
-    print("You are spending too much! Consider reducing your expenses.")
-elif spending_percentage < 30:
-    print("Great job! You are managing your expenses well.")
+print("\n ===Expense Report=== ")
+print(f"Monthly Income: {monthly_income}")
+print(f"Monthly Expenses: {monthly_expenses}")
+print(f"Savings: {savings}")
+
+expense_percentage = (monthly_expenses / monthly_income) * 100
+print(f"Percentage of Income spend: {expense_percentage:.2f}% of your income.")
+
+if expense_percentage  > 50 :
+    print("You are spending too much ")
+elif expense_percentage < 30 :
+    print("You are spending less than 30% of your income, good job!")
 else:
     print("You are spending a reasonable amount, but there's room for improvement.")
